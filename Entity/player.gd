2d9b4c2307f63861fd476f3d55a8e7adadd32ec0
Samuel Pelsy <samuel.pelsy@gmail.com>
2023-09-20 23:25:17 +0200

    can open multiple chests individually

--- Entity/player.gd
+++ Entity/player.gd
@@ -1,18 +1,22 @@
 extends KinematicBody2D
 
-signal chest_opened
+signal open_chest(chest_name)
 
 var velocity = 200
 var life
 var max_life = 20
-var in_chest_zone = false
-
 var inventory = {}
 
+var chest
+var in_chest_range
 var motion = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	for i in get_tree().get_nodes_in_group("lootChests"):
+		i.connect('in_loot_range', self, "_in_chest_range")
+		i.connect('out_loot_range', self, "_out_chest_range")
+		print('linked')
 	life = max_life
 	pass # Replace with function body.
 
@@ -26,17 +30,17 @@ func get_input(delta):
 		direction = Vector2.RIGHT
 	if Input.is_action_pressed("ui_left"):
 		direction = Vector2.LEFT
-	if Input.is_action_pressed("ui_interract") and in_chest_zone == true:
-		emit_signal("chest_opened")
+	if in_chest_range == true and Input.is_action_pressed("ui_interract"):
+		emit_signal("open_chest", chest)
 	move_and_collide(direction * velocity * delta)
 
 func _physics_process(delta):
 	get_input(delta)
 
-func _on_Area2D_body_entered(body):
-	in_chest_zone = true
-	print("entered chest zone")
-
-func _on_Area2D_body_exited(body):
-	in_chest_zone = false
-	print("exit chest zone")
+func _in_chest_range(chest_name):
+	chest = chest_name
+	in_chest_range = true
+	
+func _out_chest_range(chest_name):
+	chest = ""
+	in_chest_range = false
--- Objects/chest.gd
+++ Objects/chest.gd
@@ -1,9 +1,27 @@
 extends StaticBody2D
 
+signal in_loot_range(chest_name)
+signal out_loot_range
+
+var opened = false
+var content = {}
+var chest_name
+
 func _ready():
+	chest_name = get_name()
+	for i in get_tree().get_nodes_in_group("player"):
+		i.connect('open_chest', self, "_open")
 	$opened.visible = false
 	$closed.visible = true
 
-func _on_KinematicBody2D_chest_opened():
-	$opened.visible = true
-	$closed.visible = false
+func _open(chest_name):
+	if get_name() == chest_name:
+		$opened.visible = true
+		$closed.visible = false
+		opened = true
+
+func _on_Area2D_body_entered(body):
+	emit_signal("in_loot_range", chest_name)
+
+func _on_Area2D_body_exited(body):
+	emit_signal("out_loot_range", chest_name)
